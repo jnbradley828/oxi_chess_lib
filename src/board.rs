1a1a068b4ea7e7832d66f5fd29d11e87e7d467cc
JoshBradley <jnbradley828@gmail.com>
2025-05-09 23:09:14 -0600

    Added Cargo.lock to .gitignore. Also, partially implemented fn verify_fen() in file board.rs.

--- src/board.rs
+++ src/board.rs
@@ -1,3 +1,5 @@
+/// Struct representing a chess board.
+/// We will let the least significant bit represent the a1 square.
 pub struct ChessBoard {
     pawns: u64,
     knights: u64,
@@ -8,7 +10,7 @@ pub struct ChessBoard {
     white_pieces: u64,
     black_pieces: u64,
     side_to_move: bool,   // let true = white to move
-    en_passant: u64, // let u64 = en passant target piece (location of pawn, not capture square). Let value = 0 if no en passant is possible.
+    en_passant: u64, // let u64 = en passant target (location of capture square). Let value = 0 if no en passant is possible.
     castling_rights: u8, // uses first 4 bits (white kingside, white queenside, black kingside, black queenside)
     halfmove_clock: u8,  // tracks half moves since last capture or pawn move.
     fullmove_number: u16, // tracks full moves since start of game.
@@ -56,6 +58,94 @@ impl ChessBoard {
             fullmove_number: 1,
         }
     }
+
+    /// Creates a new chess board from a FEN string.
+    pub fn initialize_from_fen(fen: &str) -> Result<Self, String> {
+        if !verify_fen(&fen) {
+            return Err("Invalid FEN string.".to_string());
+        } else {
+            todo!("Implement initialize_from_fen.")
+        }
+    }
+}
+
+/// Verifies if a string is a valid FEN string.
+pub fn verify_fen(fen: &str) -> bool {
+    // Split whitespace and create vector of fen components.
+    let fen_components: Vec<&str> = fen.split_whitespace().collect();
+    // fen_components indices:
+    //      [0] = board position
+    //      [1] = color to move
+    //      [2] = castling rights
+    //      [3] = en passant target square
+    //      [4] = halfmove clock
+    //      [5] = fullmove number
+    // Steps to verify:
+    // Piece locations:
+    //      Split by '/' characters. If len != 8, return false.
+
+    let ranks: Vec<&str> = fen_components[0].split('/').collect();
+    if ranks.len() != 8 {
+        return false;
+    }
+
+    //      For each line, take count of alphabetical characters and add value of digits. If for any line, sum != 8, return false.
+    //      For efficiency, only check if characters are in "PNBRQKpnbrqk12345678".
+    //      Check for 1 white and 1 black king while we're at it.
+    let mut white_king_count = 0;
+    let mut black_king_count = 0;
+
+    for rank in &ranks {
+        let mut square_count = 0;
+
+        for ch in rank.chars() {
+            if "kqrbnpKQRBNP".contains(ch) {
+                square_count += 1;
+                if ch == 'K' {
+                    white_king_count += 1;
+                } else if ch == 'k' {
+                    black_king_count += 1;
+                }
+            } else if ch >= '1' && ch <= '8' {
+                square_count += ch.to_digit(10).unwrap();
+            } else {
+                return false;
+            }
+        }
+        if square_count != 8 {
+            return false;
+        }
+    }
+
+    if white_king_count != 1 {
+        return false;
+    }
+    if black_king_count != 1 {
+        return false;
+    }
+
+    // Color to move:
+    //      If not in "wb" and length 1, return false.
+    if !"wb".contains(fen_components[1]) || fen_components[1].len() != 1 {
+        return false;
+    }
+
+    // Castling rights:
+    //      If not in "KQkq-" or not len 1..4 return false.
+    if !"KQkq-".contains(fen_components[2]) || fen_components[2].len() > 4 {
+        return false;
+    }
+
+    // En passant target square:
+    //      If not '-':
+    //          If first char not "a".."z" or second char not "1".."8":
+    //              return false.
+    // Halfmove clock:
+    //      If not a non-negative integer, return false.
+    // Fullmove number:
+    //      If not a positive integer, return false.
+    // Finally: return true.
+    return true;
 }
 
 #[cfg(test)]
@@ -96,5 +186,55 @@ mod tests {
         assert_eq!(board.castling_rights, 0);
         assert_eq!(board.halfmove_clock, 0);
         assert_eq!(board.fullmove_number, 1);
+    }
+
+    #[test]
+    fn test_verify_fen() {
+        let starting_fen = verify_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"); // Starting position.
+        assert_eq!(starting_fen, true);
+
+        let empty_fen = verify_fen("8/8/8/8/8/8/8/8 w - - 0 1"); // No kings.
+        assert_eq!(empty_fen, false);
+
+        let random_fen =
+            verify_fen("1rbq1rk1/5pbp/2pNn1p1/p1Pn4/Pp1P4/1B3N1P/1P3PP1/R1BQR1K1 w - - 1 18"); // Valid FEN of a middlegame.
+        assert_eq!(random_fen, true);
+
+        let too_many_ranks_false_fen = verify_fen(
+            "1rbq1rk1/5pbp/2pNn1p1/p1Pn4/Pp1P4/1B3N1P/1P3PP1/R1BQR1K1/1rbq1rk1 w - - 1 18", // 9 ranks != 8.
+        );
+        assert_eq!(too_many_ranks_false_fen, false);
+
+        let too_few_ranks_false_fen =
+            verify_fen("1rbq1rk1/5pbp/2pNn1p1/p1Pn4/Pp1P4/1B3N1P/1P3PP1 w - - 1 18"); // 7 ranks != 7.
+        assert_eq!(too_few_ranks_false_fen, false);
+
+        let too_many_squares_false_fen =
+            verify_fen("1rbq1rk1/5pbp/2pNn1p1/p1Pn4/Pp1P4/1B3N1P/1P3PP1/R2BQR1K1 w - - 1 18"); // 1st rank has too many squares.
+        assert_eq!(too_many_squares_false_fen, false);
+
+        let too_few_squares_false_fen =
+            verify_fen("1rbq1rk1/5pbp/2pNn1p1/p1Pn4/Pp1P4/1B3N1P/1P3PP1/RBQR1K1 w - - 1 18"); // 1st rank has too few squares.
+        assert_eq!(too_few_squares_false_fen, false);
+
+        let wrong_characters_false_fen =
+            verify_fen("1rbq1ak1/5pbp/2pNn1p1/p1Pn4/Pp1P4/1B3N1P/1P3PP1/R1BQR1K1 w - - 1 18"); // 8th rank contains the char 'a'.
+        assert_eq!(wrong_characters_false_fen, false);
+
+        let invalid_color_to_move_false_fen =
+            verify_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR v KQkq - 0 1"); // Starting position with invalid color to move - 'v'.
+        assert_eq!(invalid_color_to_move_false_fen, false);
+
+        let color_to_move_len_false_fen =
+            verify_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR ww KQkq - 0 1"); // Starting position with invalid color to move - 'ww'.
+        assert_eq!(color_to_move_len_false_fen, false);
+
+        let castle_rights_len_false_fen =
+            verify_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkqK - 0 1"); // Castling rights too long.
+        assert_eq!(castle_rights_len_false_fen, false);
+
+        let castle_invalid_char_false_fen =
+            verify_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQka - 0 1"); // Invalid character in castling rights.
+        assert_eq!(castle_invalid_char_false_fen, false);
     }
 }
