61a29239586888c17d7704f6b1f7575f88927770
JoshBradley <jnbradley828@gmail.com>
2025-05-10 23:10:42 -0600

    Implemented initialize_from_fen() in board.rs, as well as unit test. (Not mentioned in earlier commits: all structs and functions in board.rs have unit tests.)

--- src/board.rs
+++ src/board.rs
@@ -11,7 +11,7 @@ pub struct ChessBoard {
     black_pieces: u64,
     side_to_move: bool,   // let true = white to move
     en_passant: u64, // let u64 = en passant target (location of capture square). Let value = 0 if no en passant is possible.
-    castling_rights: u8, // uses first 4 bits (white kingside, white queenside, black kingside, black queenside)
+    castling_rights: u8, // uses 4 least significant bits (white kingside, white queenside, black kingside, black queenside)
     halfmove_clock: u8,  // tracks half moves since last capture or pawn move.
     fullmove_number: u16, // tracks full moves since start of game.
 }
@@ -64,7 +64,93 @@ impl ChessBoard {
         if !verify_fen(&fen) {
             return Err("Invalid FEN string.".to_string());
         } else {
-            todo!("Implement initialize_from_fen.")
+            let fen_components: Vec<&str> = fen.split_whitespace().collect();
+
+            // parse fen_components[0] (piece locations).
+            let ranks: Vec<&str> = fen_components[0].split('/').collect();
+            let mut square = 0;
+
+            let mut pawns1: u64 = 0;
+            let mut knights1: u64 = 0;
+            let mut bishops1: u64 = 0;
+            let mut rooks1: u64 = 0;
+            let mut queens1: u64 = 0;
+            let mut kings1: u64 = 0;
+
+            let mut white_pieces1: u64 = 0;
+            let mut black_pieces1: u64 = 0;
+
+            for rank in ranks {
+                for ch in rank.chars().rev() {
+                    // increment by 1 for alphabetical values and set piece_type[bit] = 1,
+                    // increment by numerical for numberical values
+                    if ch.is_ascii_alphabetic() {
+                        match ch {
+                            'k' | 'K' => kings1 |= 1 << (63 - square),
+                            'q' | 'Q' => queens1 |= 1 << (63 - square),
+                            'r' | 'R' => rooks1 |= 1 << (63 - square),
+                            'b' | 'B' => bishops1 |= 1 << (63 - square),
+                            'n' | 'N' => knights1 |= 1 << (63 - square),
+                            'p' | 'P' => pawns1 |= 1 << (63 - square),
+                            _ => (),
+                        }
+                        match ch.is_uppercase() {
+                            true => white_pieces1 |= 1 << (63 - square),
+                            false => black_pieces1 |= 1 << (63 - square),
+                        }
+                        square += 1;
+                    } else {
+                        square += ch.to_digit(10).unwrap();
+                    }
+                }
+            }
+
+            // parse fen_components[2] (castling rights)
+            let mut castling_rights1: u8 = 0;
+            for ch in fen_components[2].chars() {
+                match ch {
+                    'K' => castling_rights1 |= 1 << 3,
+                    'Q' => castling_rights1 |= 1 << 2,
+                    'k' => castling_rights1 |= 1 << 1,
+                    'q' => castling_rights1 |= 1 << 0,
+                    _ => (),
+                }
+            }
+
+            // parse fen_components[3] (en passant target square)
+            let mut ep_square: u64 = 0;
+            if fen_components[3].len() == 2 {
+                let mut chs = fen_components[3].chars();
+                let ch1 = chs.next().unwrap();
+                let ch2 = chs.next().unwrap();
+                let ch1_modifier = 104 - (ch1 as u32); // formula finds how many squares to move based on the rank.
+                let ch2_modifier = 63 - (8 * ch2.to_digit(10).unwrap()); // formula finds how many squares to move based on the file.
+                let sq_modifier = ch1_modifier + ch2_modifier; // This is the index from most significant digit to switch to bit 1.
+
+                ep_square = 1 << (63 - sq_modifier);
+            }
+
+            return Ok(Self {
+                pawns: pawns1,
+                knights: knights1,
+                bishops: bishops1,
+                rooks: rooks1,
+                queens: queens1,
+                kings: kings1,
+
+                white_pieces: white_pieces1,
+                black_pieces: black_pieces1,
+
+                // parse fen_components[1] (color to move)
+                side_to_move: match fen_components[1] {
+                    "w" => true,
+                    _ => false,
+                },
+                en_passant: ep_square,
+                castling_rights: castling_rights1,
+                halfmove_clock: fen_components[4].parse::<u8>().unwrap(),
+                fullmove_number: fen_components[5].parse::<u16>().unwrap(),
+            });
         }
     }
 }
@@ -307,5 +393,98 @@ mod tests {
         let random_fen2 =
             verify_fen("rnbqk2r/pp2nppp/2pbp3/3p4/3P4/1P1BPN2/PBP2PPP/RN1QK2R b KQkq - 2 6");
         assert_eq!(random_fen2, true);
+    }
+
+    #[test]
+    fn test_intialize_from_fen() {
+        let starting_board = ChessBoard::initialize_from_fen(
+            "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+        )
+        .unwrap();
+        assert_eq!(starting_board.pawns, 0x00FF00000000FF00);
+        assert_eq!(starting_board.knights, 0x4200000000000042);
+        assert_eq!(starting_board.bishops, 0x2400000000000024);
+        assert_eq!(starting_board.rooks, 0x8100000000000081);
+        assert_eq!(starting_board.queens, 0x0800000000000008);
+        assert_eq!(starting_board.kings, 0x1000000000000010);
+        assert_eq!(starting_board.white_pieces, 0x000000000000FFFF);
+        assert_eq!(starting_board.black_pieces, 0xFFFF000000000000);
+        assert_eq!(starting_board.side_to_move, true);
+        assert_eq!(starting_board.en_passant, 0);
+        assert_eq!(starting_board.castling_rights, 0b1111);
+        assert_eq!(starting_board.halfmove_clock, 0);
+        assert_eq!(starting_board.fullmove_number, 1);
+
+        let board1 = ChessBoard::initialize_from_fen(
+            "r2qkbnr/1p2pppp/p1n5/3p4/3P1B2/3Q1N2/PPP2PPP/RN2K1R1 b Qkq - 2 8",
+        )
+        .unwrap();
+        assert_eq!(
+            board1.pawns,
+            0b0000000011110010000000010000100000001000000000001110011100000000
+        );
+        assert_eq!(
+            board1.knights,
+            0b0100000000000000000001000000000000000000001000000000000000000010
+        );
+        assert_eq!(
+            board1.bishops,
+            0b0010000000000000000000000000000000100000000000000000000000000000
+        );
+        assert_eq!(
+            board1.rooks,
+            0b1000000100000000000000000000000000000000000000000000000001000001
+        );
+        assert_eq!(
+            board1.queens,
+            0b0000100000000000000000000000000000000000000010000000000000000000
+        );
+        assert_eq!(
+            board1.kings,
+            0b0001000000000000000000000000000000000000000000000000000000010000
+        );
+        assert_eq!(
+            board1.white_pieces,
+            0b0000000000000000000000000000000000101000001010001110011101010011
+        );
+        assert_eq!(
+            board1.black_pieces,
+            0b1111100111110010000001010000100000000000000000000000000000000000
+        );
+        assert_eq!(board1.side_to_move, false);
+        assert_eq!(board1.en_passant, 0);
+        assert_eq!(board1.castling_rights, 0b0111);
+        assert_eq!(board1.halfmove_clock, 2);
+        assert_eq!(board1.fullmove_number, 8);
+
+        let board2 = ChessBoard::initialize_from_fen(
+            "rnbqkbnr/pppppppp/8/8/6pP/8/PPPPPPP1/RNBQKBNR b KQkq h3 0 1",
+        )
+        .unwrap();
+        assert_eq!(
+            board2.pawns,
+            0b0000000011111111000000000000000011000000000000000111111100000000
+        );
+        assert_eq!(board2.knights, 0x4200000000000042);
+        assert_eq!(board2.bishops, 0x2400000000000024);
+        assert_eq!(board2.rooks, 0x8100000000000081);
+        assert_eq!(board2.queens, 0x0800000000000008);
+        assert_eq!(board2.kings, 0x1000000000000010);
+        assert_eq!(
+            board2.white_pieces,
+            0b0000000000000000000000000000000010000000000000000111111111111111
+        );
+        assert_eq!(
+            board2.black_pieces,
+            0b1111111111111111000000000000000001000000000000000000000000000000
+        );
+        assert_eq!(board2.side_to_move, false);
+        assert_eq!(
+            board2.en_passant,
+            0b0000000000000000000000000000000000000001000000000000000000000000
+        );
+        assert_eq!(board2.castling_rights, 0b1111);
+        assert_eq!(board2.halfmove_clock, 0);
+        assert_eq!(board2.fullmove_number, 1);
     }
 }
