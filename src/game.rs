09df284fb758807740887d052d491366e6165f8d
JoshBradley <jnbradley828@gmail.com>
2026-03-04 22:09:50 -0700

    set up outline for ChessGame struct and GameResult enum

--- src/game.rs
+++ src/game.rs
@@ -1,8 +1,32 @@
 use crate::board;
+use crate::moves;
 use rustc_hash::FxHashMap;
 
 pub struct ChessGame {
     pub board: board::ChessBoard,
+    pub time_control: (u32, u32), // starting_ms, increment_ms
     pub moves: Vec<(u16, board::UndoInfo)>,
     pub positions_count: FxHashMap<u64, u8>,
+    pub legal_moves: Vec<u16>,
+    pub result: GameResult,
+}
+
+pub enum GameResult {
+    InProgress,
+    WhiteWins(WinReason),
+    BlackWins(WinReason),
+    Draw(DrawReason),
+}
+
+pub enum WinReason {
+    Checkmate,
+    Resignation,
+    Timeout,
+}
+
+pub enum DrawReason {
+    Stalemate,
+    FiftyMoveRule,
+    ThreefoldRepitition,
+    InsufficientMaterial,
 }
