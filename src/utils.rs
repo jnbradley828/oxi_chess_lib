521d8df4d329e0d7e5c6d4ece07930c5cfe5bc20
JoshBradley <jnbradley828@gmail.com>
2026-03-08 23:20:50 -0600

    Put tests in test module so they are not included in production binary.

--- src/utils.rs
+++ src/utils.rs
@@ -256,345 +256,349 @@ pub fn decode_move(move_i: u16) -> [u8; 3] {
 }
 
 // Unit Tests
-
-const A_SQUARES: [u64; 8] = [
-    0x0000000000000001,
-    0x0000000000000100,
-    0x0000000000010000,
-    0x0000000001000000,
-    0x0000000100000000,
-    0x0000010000000000,
-    0x0001000000000000,
-    0x0100000000000000,
-];
-#[test]
-fn test_on_a_file() {
-    for i in 0..64 {
-        let square: u64 = 1 << i;
-        if A_SQUARES.contains(&square) {
-            assert_eq!(on_a_file(square), true);
-        } else {
-            assert_eq!(on_a_file(square), false);
-        }
-    }
-}
-
-#[test]
-fn test_on_b_file() {
-    let b_squares: [u64; 8] = A_SQUARES.map(|x| x << 1);
-
-    for i in 0..64 {
-        let square: u64 = 1 << i;
-        if b_squares.contains(&square) {
-            assert_eq!(on_b_file(square), true);
-        } else {
-            assert_eq!(on_b_file(square), false);
-        }
-    }
-}
-
-#[test]
-fn test_on_c_file() {
-    let c_squares: [u64; 8] = A_SQUARES.map(|x| x << 2);
-
-    for i in 0..64 {
-        let square: u64 = 1 << i;
-        if c_squares.contains(&square) {
-            assert_eq!(on_c_file(square), true);
-        } else {
-            assert_eq!(on_c_file(square), false);
-        }
-    }
-}
-
-#[test]
-fn test_on_d_file() {
-    let d_squares: [u64; 8] = A_SQUARES.map(|x| x << 3);
-
-    for i in 0..64 {
-        let square: u64 = 1 << i;
-        if d_squares.contains(&square) {
-            assert_eq!(on_d_file(square), true);
-        } else {
-            assert_eq!(on_d_file(square), false);
-        }
-    }
-}
-
-#[test]
-fn test_on_e_file() {
-    let e_squares: [u64; 8] = A_SQUARES.map(|x| x << 4);
-
-    for i in 0..64 {
-        let square: u64 = 1 << i;
-        if e_squares.contains(&square) {
-            assert_eq!(on_e_file(square), true);
-        } else {
-            assert_eq!(on_e_file(square), false);
-        }
-    }
-}
-
-#[test]
-fn test_on_f_file() {
-    let f_squares: [u64; 8] = A_SQUARES.map(|x| x << 5);
-
-    for i in 0..64 {
-        let square: u64 = 1 << i;
-        if f_squares.contains(&square) {
-            assert_eq!(on_f_file(square), true);
-        } else {
-            assert_eq!(on_f_file(square), false);
-        }
-    }
-}
-
-#[test]
-fn test_on_g_file() {
-    let g_squares: [u64; 8] = A_SQUARES.map(|x| x << 6);
-
-    for i in 0..64 {
-        let square: u64 = 1 << i;
-        if g_squares.contains(&square) {
-            assert_eq!(on_g_file(square), true);
-        } else {
-            assert_eq!(on_g_file(square), false);
-        }
-    }
-}
-
-#[test]
-fn test_on_h_file() {
-    let h_squares: [u64; 8] = A_SQUARES.map(|x| x << 7);
-
-    for i in 0..64 {
-        let square: u64 = 1 << i;
-        if h_squares.contains(&square) {
-            assert_eq!(on_h_file(square), true);
-        } else {
-            assert_eq!(on_h_file(square), false);
-        }
-    }
-}
-
-#[test]
-fn test_on_rank_1() {
-    for i in 0..8 {
-        let square: u64 = 1 << i;
-        assert_eq!(on_rank_1(square), true);
-    }
-    for i in 8..64 {
-        let square: u64 = 1 << i;
-        assert_eq!(on_rank_1(square), false);
-    }
-}
-
-#[test]
-fn test_on_rank_2() {
-    for i in (0..8).chain(16..64) {
-        let square: u64 = 1 << i;
-        assert_eq!(on_rank_2(square), false);
-    }
-    for i in 8..16 {
-        let square: u64 = 1 << i;
-        assert_eq!(on_rank_2(square), true);
-    }
-}
-
-#[test]
-fn test_on_rank_3() {
-    for i in (0..16).chain(24..64) {
-        let square: u64 = 1 << i;
-        assert_eq!(on_rank_3(square), false);
-    }
-    for i in 16..24 {
-        let square: u64 = 1 << i;
-        assert_eq!(on_rank_3(square), true);
-    }
-}
-
-#[test]
-fn test_on_rank_4() {
-    for i in (0..24).chain(32..64) {
-        let square: u64 = 1 << i;
-        assert_eq!(on_rank_4(square), false);
-    }
-    for i in 24..32 {
-        let square: u64 = 1 << i;
-        assert_eq!(on_rank_4(square), true);
-    }
-}
-
-#[test]
-fn test_on_rank_5() {
-    for i in (0..32).chain(40..64) {
-        let square: u64 = 1 << i;
-        assert_eq!(on_rank_5(square), false);
-    }
-    for i in 32..40 {
-        let square: u64 = 1 << i;
-        assert_eq!(on_rank_5(square), true);
-    }
-}
-
-#[test]
-fn test_on_rank_6() {
-    for i in (0..40).chain(48..64) {
-        let square: u64 = 1 << i;
-        assert_eq!(on_rank_6(square), false);
-    }
-    for i in 40..48 {
-        let square: u64 = 1 << i;
-        assert_eq!(on_rank_6(square), true);
-    }
-}
-
-#[test]
-fn test_on_rank_7() {
-    for i in (0..48).chain(56..64) {
-        let square: u64 = 1 << i;
-        assert_eq!(on_rank_7(square), false);
-    }
-    for i in 48..56 {
-        let square: u64 = 1 << i;
-        assert_eq!(on_rank_7(square), true);
-    }
-}
-
-#[test]
-fn test_on_rank_8() {
-    for i in 0..56 {
-        let square: u64 = 1 << i;
-        assert_eq!(on_rank_8(square), false);
-    }
-    for i in 56..64 {
-        let square: u64 = 1 << i;
-        assert_eq!(on_rank_8(square), true);
-    }
-}
-
-#[test]
-fn test_bb_to_square() {
-    // a1
-    let board1: u64 = 1;
-    assert_eq!(bb_to_square(&board1).unwrap(), "a1");
-    // e4
-    let board2: u64 = 0x0000000010000000;
-    assert_eq!(bb_to_square(&board2).unwrap(), "e4");
-    // h8
-    let board3: u64 = 0x8000000000000000;
-    assert_eq!(bb_to_square(&board3).unwrap(), "h8");
-    // c7
-    let board4: u64 = 0x0004000000000000;
-    assert_eq!(bb_to_square(&board4).unwrap(), "c7");
-    // invalid: too many squares
-    let board5: u64 = 3;
-    assert_eq!(
-        bb_to_square(&board5),
-        Err("Invalid bitboard: more than one piece on the board.".to_string())
-    );
-}
-
-#[test]
-fn test_square_to_bb() {
-    // a1
-    let square1: &str = "a1";
-    assert_eq!(square_to_bb(square1).unwrap(), 1);
-    // e4
-    let square2: &str = "e4";
-    assert_eq!(square_to_bb(square2).unwrap(), 0x0000000010000000);
-    // h8
-    let square3: &str = "h8";
-    assert_eq!(square_to_bb(square3).unwrap(), 0x8000000000000000);
-    // c7
-    let square4: &str = "c7";
-    assert_eq!(square_to_bb(square4).unwrap(), 0x0004000000000000);
-    // invalid: file out of range
-    let square5: &str = "i1";
-    assert_eq!(
-        square_to_bb(square5),
-        Err("Invalid square: invalid file or rank.".to_string())
-    );
-    // invalid: rank out of range
-    let square6: &str = "a9";
-    assert_eq!(
-        square_to_bb(square6),
-        Err("Invalid square: invalid file or rank.".to_string())
-    );
-    // invalid: too many chars
-    let square7: &str = "a11";
-    assert_eq!(
-        square_to_bb(square7),
-        Err("Invalid square: too many characters.".to_string())
-    );
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    const A_SQUARES: [u64; 8] = [
+        0x0000000000000001,
+        0x0000000000000100,
+        0x0000000000010000,
+        0x0000000001000000,
+        0x0000000100000000,
+        0x0000010000000000,
+        0x0001000000000000,
+        0x0100000000000000,
+    ];
     #[test]
-    fn test_squares_above() {
+    fn test_on_a_file() {
+        for i in 0..64 {
+            let square: u64 = 1 << i;
+            if A_SQUARES.contains(&square) {
+                assert_eq!(on_a_file(square), true);
+            } else {
+                assert_eq!(on_a_file(square), false);
+            }
+        }
+    }
+
+    #[test]
+    fn test_on_b_file() {
+        let b_squares: [u64; 8] = A_SQUARES.map(|x| x << 1);
+
+        for i in 0..64 {
+            let square: u64 = 1 << i;
+            if b_squares.contains(&square) {
+                assert_eq!(on_b_file(square), true);
+            } else {
+                assert_eq!(on_b_file(square), false);
+            }
+        }
+    }
+
+    #[test]
+    fn test_on_c_file() {
+        let c_squares: [u64; 8] = A_SQUARES.map(|x| x << 2);
+
+        for i in 0..64 {
+            let square: u64 = 1 << i;
+            if c_squares.contains(&square) {
+                assert_eq!(on_c_file(square), true);
+            } else {
+                assert_eq!(on_c_file(square), false);
+            }
+        }
+    }
+
+    #[test]
+    fn test_on_d_file() {
+        let d_squares: [u64; 8] = A_SQUARES.map(|x| x << 3);
+
+        for i in 0..64 {
+            let square: u64 = 1 << i;
+            if d_squares.contains(&square) {
+                assert_eq!(on_d_file(square), true);
+            } else {
+                assert_eq!(on_d_file(square), false);
+            }
+        }
+    }
+
+    #[test]
+    fn test_on_e_file() {
+        let e_squares: [u64; 8] = A_SQUARES.map(|x| x << 4);
+
+        for i in 0..64 {
+            let square: u64 = 1 << i;
+            if e_squares.contains(&square) {
+                assert_eq!(on_e_file(square), true);
+            } else {
+                assert_eq!(on_e_file(square), false);
+            }
+        }
+    }
+
+    #[test]
+    fn test_on_f_file() {
+        let f_squares: [u64; 8] = A_SQUARES.map(|x| x << 5);
+
+        for i in 0..64 {
+            let square: u64 = 1 << i;
+            if f_squares.contains(&square) {
+                assert_eq!(on_f_file(square), true);
+            } else {
+                assert_eq!(on_f_file(square), false);
+            }
+        }
+    }
+
+    #[test]
+    fn test_on_g_file() {
+        let g_squares: [u64; 8] = A_SQUARES.map(|x| x << 6);
+
+        for i in 0..64 {
+            let square: u64 = 1 << i;
+            if g_squares.contains(&square) {
+                assert_eq!(on_g_file(square), true);
+            } else {
+                assert_eq!(on_g_file(square), false);
+            }
+        }
+    }
+
+    #[test]
+    fn test_on_h_file() {
+        let h_squares: [u64; 8] = A_SQUARES.map(|x| x << 7);
+
+        for i in 0..64 {
+            let square: u64 = 1 << i;
+            if h_squares.contains(&square) {
+                assert_eq!(on_h_file(square), true);
+            } else {
+                assert_eq!(on_h_file(square), false);
+            }
+        }
+    }
+
+    #[test]
+    fn test_on_rank_1() {
+        for i in 0..8 {
+            let square: u64 = 1 << i;
+            assert_eq!(on_rank_1(square), true);
+        }
+        for i in 8..64 {
+            let square: u64 = 1 << i;
+            assert_eq!(on_rank_1(square), false);
+        }
+    }
+
+    #[test]
+    fn test_on_rank_2() {
+        for i in (0..8).chain(16..64) {
+            let square: u64 = 1 << i;
+            assert_eq!(on_rank_2(square), false);
+        }
+        for i in 8..16 {
+            let square: u64 = 1 << i;
+            assert_eq!(on_rank_2(square), true);
+        }
+    }
+
+    #[test]
+    fn test_on_rank_3() {
+        for i in (0..16).chain(24..64) {
+            let square: u64 = 1 << i;
+            assert_eq!(on_rank_3(square), false);
+        }
+        for i in 16..24 {
+            let square: u64 = 1 << i;
+            assert_eq!(on_rank_3(square), true);
+        }
+    }
+
+    #[test]
+    fn test_on_rank_4() {
+        for i in (0..24).chain(32..64) {
+            let square: u64 = 1 << i;
+            assert_eq!(on_rank_4(square), false);
+        }
+        for i in 24..32 {
+            let square: u64 = 1 << i;
+            assert_eq!(on_rank_4(square), true);
+        }
+    }
+
+    #[test]
+    fn test_on_rank_5() {
+        for i in (0..32).chain(40..64) {
+            let square: u64 = 1 << i;
+            assert_eq!(on_rank_5(square), false);
+        }
+        for i in 32..40 {
+            let square: u64 = 1 << i;
+            assert_eq!(on_rank_5(square), true);
+        }
+    }
+
+    #[test]
+    fn test_on_rank_6() {
+        for i in (0..40).chain(48..64) {
+            let square: u64 = 1 << i;
+            assert_eq!(on_rank_6(square), false);
+        }
+        for i in 40..48 {
+            let square: u64 = 1 << i;
+            assert_eq!(on_rank_6(square), true);
+        }
+    }
+
+    #[test]
+    fn test_on_rank_7() {
+        for i in (0..48).chain(56..64) {
+            let square: u64 = 1 << i;
+            assert_eq!(on_rank_7(square), false);
+        }
+        for i in 48..56 {
+            let square: u64 = 1 << i;
+            assert_eq!(on_rank_7(square), true);
+        }
+    }
+
+    #[test]
+    fn test_on_rank_8() {
+        for i in 0..56 {
+            let square: u64 = 1 << i;
+            assert_eq!(on_rank_8(square), false);
+        }
+        for i in 56..64 {
+            let square: u64 = 1 << i;
+            assert_eq!(on_rank_8(square), true);
+        }
+    }
+
+    #[test]
+    fn test_bb_to_square() {
+        // a1
+        let board1: u64 = 1;
+        assert_eq!(bb_to_square(&board1).unwrap(), "a1");
+        // e4
+        let board2: u64 = 0x0000000010000000;
+        assert_eq!(bb_to_square(&board2).unwrap(), "e4");
+        // h8
+        let board3: u64 = 0x8000000000000000;
+        assert_eq!(bb_to_square(&board3).unwrap(), "h8");
+        // c7
+        let board4: u64 = 0x0004000000000000;
+        assert_eq!(bb_to_square(&board4).unwrap(), "c7");
+        // invalid: too many squares
+        let board5: u64 = 3;
+        assert_eq!(
+            bb_to_square(&board5),
+            Err("Invalid bitboard: more than one piece on the board.".to_string())
+        );
+    }
+
+    #[test]
+    fn test_square_to_bb() {
         // a1
         let square1: &str = "a1";
-        let sq1: u64 = square_to_bb(square1).unwrap();
-        assert_eq!(squares_above(&sq1), 0xFFFFFFFFFFFFFF00);
-        // g7
-        let square2: &str = "g7";
-        let sq2: u64 = square_to_bb(square2).unwrap();
-        assert_eq!(squares_above(&sq2), 0xFF00000000000000);
+        assert_eq!(square_to_bb(square1).unwrap(), 1);
+        // e4
+        let square2: &str = "e4";
+        assert_eq!(square_to_bb(square2).unwrap(), 0x0000000010000000);
+        // h8
+        let square3: &str = "h8";
+        assert_eq!(square_to_bb(square3).unwrap(), 0x8000000000000000);
+        // c7
+        let square4: &str = "c7";
+        assert_eq!(square_to_bb(square4).unwrap(), 0x0004000000000000);
+        // invalid: file out of range
+        let square5: &str = "i1";
+        assert_eq!(
+            square_to_bb(square5),
+            Err("Invalid square: invalid file or rank.".to_string())
+        );
+        // invalid: rank out of range
+        let square6: &str = "a9";
+        assert_eq!(
+            square_to_bb(square6),
+            Err("Invalid square: invalid file or rank.".to_string())
+        );
+        // invalid: too many chars
+        let square7: &str = "a11";
+        assert_eq!(
+            square_to_bb(square7),
+            Err("Invalid square: too many characters.".to_string())
+        );
     }
 
-    #[test]
-    fn test_squares_below() {
-        // a1
-        let square1: &str = "a1";
-        let sq1: u64 = square_to_bb(square1).unwrap();
-        assert_eq!(squares_below(&sq1), 0);
-        // g7
-        let square2: &str = "g7";
-        let sq2: u64 = square_to_bb(square2).unwrap();
-        assert_eq!(squares_below(&sq2), 0x0000FFFFFFFFFFFF);
-    }
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+        #[test]
+        fn test_squares_above() {
+            // a1
+            let square1: &str = "a1";
+            let sq1: u64 = square_to_bb(square1).unwrap();
+            assert_eq!(squares_above(&sq1), 0xFFFFFFFFFFFFFF00);
+            // g7
+            let square2: &str = "g7";
+            let sq2: u64 = square_to_bb(square2).unwrap();
+            assert_eq!(squares_above(&sq2), 0xFF00000000000000);
+        }
 
-    #[test]
-    fn test_squares_left() {
-        // a1
-        let square1: &str = "a1";
-        let sq1: u64 = square_to_bb(square1).unwrap();
-        assert_eq!(squares_left(&sq1), 0);
-        // g7
-        let square2: &str = "g7";
-        let sq2: u64 = square_to_bb(square2).unwrap();
-        assert_eq!(squares_left(&sq2), 0x3F3F3F3F3F3F3F3F);
-    }
+        #[test]
+        fn test_squares_below() {
+            // a1
+            let square1: &str = "a1";
+            let sq1: u64 = square_to_bb(square1).unwrap();
+            assert_eq!(squares_below(&sq1), 0);
+            // g7
+            let square2: &str = "g7";
+            let sq2: u64 = square_to_bb(square2).unwrap();
+            assert_eq!(squares_below(&sq2), 0x0000FFFFFFFFFFFF);
+        }
 
-    #[test]
-    fn test_squares_right() {
-        // a1
-        let square1: &str = "a1";
-        let sq1: u64 = square_to_bb(square1).unwrap();
-        assert_eq!(squares_right(&sq1), 0xFEFEFEFEFEFEFEFE);
-        // g7
-        let square2: &str = "g7";
-        let sq2: u64 = square_to_bb(square2).unwrap();
-        assert_eq!(squares_right(&sq2), 0x8080808080808080);
-    }
+        #[test]
+        fn test_squares_left() {
+            // a1
+            let square1: &str = "a1";
+            let sq1: u64 = square_to_bb(square1).unwrap();
+            assert_eq!(squares_left(&sq1), 0);
+            // g7
+            let square2: &str = "g7";
+            let sq2: u64 = square_to_bb(square2).unwrap();
+            assert_eq!(squares_left(&sq2), 0x3F3F3F3F3F3F3F3F);
+        }
 
-    #[test]
-    fn test_encode_move() {
-        let from: u8 = 30;
-        let to: u8 = 50;
-        let flag: u8 = 8;
+        #[test]
+        fn test_squares_right() {
+            // a1
+            let square1: &str = "a1";
+            let sq1: u64 = square_to_bb(square1).unwrap();
+            assert_eq!(squares_right(&sq1), 0xFEFEFEFEFEFEFEFE);
+            // g7
+            let square2: &str = "g7";
+            let sq2: u64 = square_to_bb(square2).unwrap();
+            assert_eq!(squares_right(&sq2), 0x8080808080808080);
+        }
 
-        let encoded_move = encode_move(from, to, flag);
-        assert_eq!(encoded_move, 0b0111101100101000);
-    }
+        #[test]
+        fn test_encode_move() {
+            let from: u8 = 30;
+            let to: u8 = 50;
+            let flag: u8 = 8;
 
-    #[test]
-    fn test_decode_move() {
-        let move_i = 0b1100110101011011;
-        let move_i_decoded: [u8; 3] = [0b110011, 0b010101, 0b1011];
+            let encoded_move = encode_move(from, to, flag);
+            assert_eq!(encoded_move, 0b0111101100101000);
+        }
 
-        assert_eq!(decode_move(move_i), move_i_decoded);
+        #[test]
+        fn test_decode_move() {
+            let move_i = 0b1100110101011011;
+            let move_i_decoded: [u8; 3] = [0b110011, 0b010101, 0b1011];
+
+            assert_eq!(decode_move(move_i), move_i_decoded);
+        }
     }
 }
